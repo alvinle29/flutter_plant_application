c5e63de529f135af956cd8435a6a007b80a92881
alvinle29 <bach938@gmail.com>
2023-05-16 16:44:17 +0700

    added search bar for home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_plant_application/constants/constants.dart';
+
+import '../models/plants.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,10 +13,68 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    int selectedIndex = 0;
+    Size size = MediaQuery.of(context).size;
+
+    List<Plant> _plantList = Plant.plantList;
+
+    List<String> _plantTypes = [
+      'Recommended',
+      'Indoor',
+      'Outdoor',
+      'Garden',
+      'Supplement',
+    ];
+
+    //Toggle Fav
+    bool toggleIsFavorite(bool isFavorite) {
+      return !isFavorite;
+    }
+
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen')
-      )
-    );
+        body: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                  ),
+                  width: size.width * .9,
+                  decoration: BoxDecoration(
+                    color: Constants.primaryColor.withOpacity(.1),
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.search,
+                        color: Colors.black54.withOpacity(.6),
+                      ),
+                      const Expanded(
+                          child: TextField(
+                        showCursor: false,
+                        decoration: InputDecoration(
+                            hintText: 'Search Plant',
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none),
+                      )),
+                      Icon(Icons.mic, color: Colors.black54.withOpacity(.6))
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
