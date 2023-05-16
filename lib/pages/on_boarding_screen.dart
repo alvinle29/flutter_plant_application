cabacc21335f2c395b6e903b7759eb7b5a27e7ce
alvinle29 <bach938@gmail.com>
2023-05-16 13:59:57 +0700

    finished boarding page

--- lib/pages/on_boarding_screen.dart
+++ lib/pages/on_boarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_plant_application/constants/constants.dart';
+import 'login_page.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
@@ -39,53 +40,150 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
         alignment: Alignment.bottomCenter,
         children: [
           PageView(
-            onPageChanged: (int page) => {
+            onPageChanged: (int page) =>
+            {
               setState(
-                () {
+                    () {
                   currentIndex = page;
                 },
               )
             },
             controller: _pageController,
             children: [
-              Container(
-                padding: const EdgeInsets.only(left: 50, right: 50, bottom: 80),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 350,
-                        child: Image.asset('assets/images/plant-one.png'),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        Constants.titleOne,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Constants.primaryColor,
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        Constants.descriptionOne,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w400),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                    ]),
+              createPage(
+                image: 'assets/images/plant-one.png',
+                title: Constants.titleOne,
+                description: Constants.descriptionOne,
+              ),
+              createPage(
+                image: 'assets/images/plant-two.png',
+                title: Constants.titleTwo,
+                description: Constants.descriptionTwo,
+              ),
+              createPage(
+                image: 'assets/images/plant-three.png',
+                title: Constants.titleThree,
+                description: Constants.descriptionThree,
               )
             ],
+          ),
+          Positioned(
+              bottom: 80,
+              left: 30,
+              child: Row(
+                children: _buildIndicator(),
+              )),
+          Positioned(
+            bottom: 60,
+            right: 30,
+            child: Container(
+              padding: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Constants.primaryColor
+              ),
+              child: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      if (currentIndex < 2) {
+                        currentIndex++;
+                        if (currentIndex < 3) {
+                          _pageController.nextPage(
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.easeIn);
+                        }
+                      } else {
+                        Navigator.pushReplacement(
+                            context, MaterialPageRoute(builder: (
+                            _) => const Login()));
+                      }
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 24,
+                    color: Colors.white,
+                  )),
+            ),
           )
         ],
       ),
+    );
+  }
+
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: 10.0,
+      width: isActive ? 20 : 8,
+      margin: const EdgeInsets.only(right: 5.0),
+      decoration: BoxDecoration(
+        color: Constants.primaryColor,
+        borderRadius: BorderRadius.circular(5),
+      ),
+    );
+  }
+
+  List<Widget> _buildIndicator() {
+    List<Widget> indicators = [];
+
+    for (int i = 0; i < 3; i++) {
+      if (currentIndex == i) {
+        indicators.add(_indicator(true));
+      } else {
+        indicators.add(_indicator(false));
+      }
+    }
+
+    return indicators;
+  }
+}
+
+class createPage extends StatelessWidget {
+  final String image;
+  final String title;
+  final String description;
+
+  const createPage({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 50, right: 50, bottom: 80),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        SizedBox(
+          height: 350,
+          child: Image.asset(image),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Constants.primaryColor,
+              fontSize: 30,
+              fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          description,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+              fontSize: 20, fontWeight: FontWeight.w400, color: Colors.grey),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+      ]),
     );
   }
 }
