8ba445d12b2cbec07b7716cc9c2c30a0c831293f
alvinle29 <bach938@gmail.com>
2023-05-16 15:43:09 +0700

    added navBar

--- lib/screens/root_screen.dart
+++ lib/screens/root_screen.dart
@@ -1,4 +1,12 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_plant_application/constants/constants.dart';
+import 'package:flutter_plant_application/screens/cart_screen.dart';
+import 'package:flutter_plant_application/screens/favorite_screen.dart';
+import 'package:flutter_plant_application/screens/home_screen.dart';
+import 'package:flutter_plant_application/screens/profile_screen.dart';
+import 'package:flutter_plant_application/screens/scan_screen.dart';
+import 'package:page_transition/page_transition.dart';
 
 class RootPage extends StatefulWidget {
   const RootPage({Key? key}) : super(key: key);
@@ -8,11 +16,80 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  int _bottomNavIndex = 0;
+
+  List<Widget> pages = const [
+    HomeScreen(),
+    FavoriteScreen(),
+    CartScreen(),
+    ProfileScreen()
+  ];
+
+  List<IconData> iconList = [
+    Icons.home,
+    Icons.favorite,
+    Icons.shopping_cart,
+    Icons.person
+  ];
+
+  List<String> titleList = ['Home', 'Favorite', 'Cart', 'Profile'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Root Page')
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              titleList[_bottomNavIndex],
+              style: TextStyle(
+                  color: Constants.blackColor,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 24),
+            ),
+            Icon(
+              Icons.notifications,
+              color: Constants.blackColor,
+              size: 30.0,
+            )
+          ],
+        ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0.0,
+      ),
+      body: IndexedStack(
+        index: _bottomNavIndex,
+        children: pages,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              PageTransition(
+                  child: const ScanScreen(),
+                  type: PageTransitionType.bottomToTop));
+        },
+        backgroundColor: Constants.primaryColor,
+        child: Image.asset(
+          'assets/images/code-scan-two.png',
+          height: 30.0,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        splashColor: Constants.primaryColor,
+        activeColor: Constants.primaryColor,
+        inactiveColor: Colors.black.withOpacity(.5),
+        icons: iconList,
+        activeIndex: _bottomNavIndex,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.softEdge,
+        onTap: (index) {
+          setState(() {
+            _bottomNavIndex = index;
+          });
+        },
       ),
     );
   }
